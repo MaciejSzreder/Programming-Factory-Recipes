d50d4934e12abc19777d5c5b55c1526a1b9db262
Maciej Szreder <mszreder3.14@wp.pl>
2025-02-07 01:39:39 +0100

    separate operations interface and implementation

--- operations.hpp
+++ operations.hpp
@@ -1,8 +1,3 @@
 #pragma once
 
-#include"operations/add.hpp"
-#include"operations/ascii.hpp"
-#include"operations/cutter.hpp"
-#include"operations/indexof.hpp"
-#include"operations/remainder.hpp"
-#include"operations/square.hpp"
+#include"operations/register.hpp"
--- operations/add.cpp
+++ operations/add.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include<string>
 #include<format>
 #include<cmath>
