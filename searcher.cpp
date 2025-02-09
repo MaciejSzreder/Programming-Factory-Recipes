d812bf9a9951b0cea3695b921f6328e5fb1fe093
Maciej Szreder <mszreder3.14@wp.pl>
2025-02-09 22:16:22 +0100

    separate searcher interface and implementation

---
+++ searcher.cpp
@@ -0,0 +1,89 @@
+#include<algorithm>
+
+#include"recipe.hpp"
+#include"operations.hpp"
+
+using operations::Operations;
+using operations::OperationList;
+
+#include"searcher.hpp"
+
+void Searcher::add(Value value)
+{
+	creatable.push_back({.value = value});
+}
+
+void Searcher::add(Operations::Definition operation)
+{
+	operations.push_back(operation);
+}
+
+void Searcher::moreRecipes()
+{
+	const OperationList &operationList = operations.empty() ? Operations::definitions : operations;
+	if(creatable.size()==0){
+		add(Value(Value::Number(0)));
+	}
+	try{
+		creatable.reserve(creatable.size()+creatable.size()*creatable.size()*operationList.size());
+	}catch(std::bad_alloc&){
+		// this optimizes allocation, might not all will be used
+	}
+	try{
+		std::span origin(creatable);
+		for(const auto &operation: operationList){
+			if(operation.arity == 1){
+				for(const auto &first: origin){
+					Value value = operation.eval({first.value});
+					if(!value.empty()){
+						creatable.push_back(Recipe{
+							.value = value,
+							.operation = &operation,
+							.recipients = {first.value}
+						});
+					}
+				}
+			} else if(operation.arity == 2){	
+				for(auto &first: origin){
+					for(const auto &second: origin){
+						Value value = operation.eval({first.value, second.value});
+						if(!value.empty()){
+							creatable.push_back(Recipe{
+								.value = value,
+								.operation = &operation,
+								.recipients = {first.value,second.value}
+							});
+						}
+					}
+				}
+			} else {
+				throw "Arity " + std::to_string(operation.arity) + " is not implemented";
+			}
+		}
+	}catch(std::bad_alloc){
+		throw "Not enough memory to create now recipes";
+	}
+	std::ranges::stable_sort(creatable,Value::Order(),&Recipe::value);
+	creatable.erase(std::ranges::unique(creatable,Value::Equality(),&Recipe::value).begin(),creatable.end());
+}
+
+Recipe Searcher::find(Value value)
+{
+	for(;;){
+		auto recipe = std::ranges::find_if(
+			creatable,[=](const Recipe& recipe){
+				return recipe.value == value || recipe.value!=recipe.value && value!=value;
+			}
+		);
+		if(recipe != creatable.end()){
+			return *recipe;
+		}
+		moreRecipes();
+	}
+}
+
+void Searcher::clear()
+{
+	creatable.clear();
+	operations.clear();
+}
--- searcher.hpp
+++ searcher.hpp
@@ -11,83 +11,12 @@ struct Searcher
 	Recipes creatable;
 	OperationList operations;
 
-	void add(Value value)
-	{
-		creatable.push_back({.value = value});
-	}
-
-	void add(Operations::Definition operation)
-	{
-		operations.push_back(operation);
-	}
+	void add(Value value);
+	void add(Operations::Definition operation);
 	
-	void moreRecipes()
-	{
-		const OperationList &operationList = operations.empty() ? Operations::definitions : operations;
-		if(creatable.size()==0){
-			add(Value(Value::Number(0)));
-		}
-		try{
-			creatable.reserve(creatable.size()+creatable.size()*creatable.size()*operationList.size());
-		}catch(std::bad_alloc&){
-			// this optimizes allocation, might not all will be used
-		}
-		try{
-			std::span origin(creatable);
-			for(const auto &operation: operationList){
-				if(operation.arity == 1){
-					for(const auto &first: origin){
-						Value value = operation.eval({first.value});
-						if(!value.empty()){
-							creatable.push_back(Recipe{
-								.value = value,
-								.operation = &operation,
-								.recipients = {first.value}
-							});
-						}
-					}
-				} else if(operation.arity == 2){	
-					for(auto &first: origin){
-						for(const auto &second: origin){
-							Value value = operation.eval({first.value, second.value});
-							if(!value.empty()){
-								creatable.push_back(Recipe{
-									.value = value,
-									.operation = &operation,
-									.recipients = {first.value,second.value}
-								});
-							}
-						}
-					}
-				} else {
-					throw "Arity " + std::to_string(operation.arity) + " is not implemented";
-				}
-			}
-		}catch(std::bad_alloc){
-			throw "Not enough memory to create now recipes";
-		}
-		std::ranges::stable_sort(creatable,Value::Order(),&Recipe::value);
-		creatable.erase(std::ranges::unique(creatable,Value::Equality(),&Recipe::value).begin(),creatable.end());
-	}
+	void moreRecipes();
 
-	Recipe find(Value value)
-	{
-		for(;;){
-			auto recipe = std::ranges::find_if(
-				creatable,[=](const Recipe& recipe){
-					return recipe.value == value || recipe.value!=recipe.value && value!=value;
-				}
-			);
-			if(recipe != creatable.end()){
-				return *recipe;
-			}
-			moreRecipes();
-		}
-	}
+	Recipe find(Value value);
 
-	void clear()
-	{
-		creatable.clear();
-		operations.clear();
-	}
+	void clear();
 };
