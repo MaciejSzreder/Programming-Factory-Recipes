9c1f6ea6e50e5bcc07eee299c7ed4d30039d67af
Maciej Szreder <mszreder3.14@wp.pl>
2025-02-10 19:29:38 +0100

    separate interpreter interface and implementation

---
+++ interpreter.cpp
@@ -0,0 +1,56 @@
+#include<string>
+#include<charconv>
+
+#include"utils.hpp"
+#include"recipe.hpp"
+#include"parser.hpp"
+#include"searcher.hpp"
+#include"commands.hpp"
+#include"interpreter.hpp"
+
+using namespace parser;
+using namespace parser::literals;
+
+Interpreter::Command Interpreter::parse_command(std::istream& in)
+{
+	Command command;
+	std::string line;
+	std::getline(in,line);
+	
+	auto number = FromChars<float>();
+	auto string = Quoted() | Quoted('\'');
+	auto identifier = "[^ \t\n\r'\"][^ \t\n\r]*"_re;
+	auto commands = utils::map(commands::Commands::definitions, [](auto definition){return definition.name;});
+	
+	auto tokens = (Keyword::keywords(commands)+*(number|identifier|string)).parse(line);
+
+	command.type = tokens.get<0>().match;
+	
+	auto arguments = utils::map(tokens.get<1>().parsed,[=](auto argument)->commands::Commands::Definition::Argument{
+		if(argument == number){
+			return Value(std::any_cast<float>(argument.value));
+		}else if(argument == string){
+			return Value(std::any_cast<std::string>(argument.value));
+		}else if(argument == identifier){
+			return argument.match;
+		}else{
+			throw "unexpected token format";
+		}
+	});
+	command.arguments.assign(arguments.begin(),arguments.end());
+
+	return command;
+}
+
+void Interpreter::execute_command(Command &command, std::ostream& out)
+{
+	commands::Commands::find(command.type)->execute(searcher,command.arguments);
+}
+
+void Interpreter::run()
+{
+	for(;;){
+		auto command = parse_command(in);
+		execute_command(command,out);
+	}
+}
--- interpreter.hpp
+++ interpreter.hpp
@@ -1,25 +1,18 @@
 #pragma once
 
 #include<string>
-#include<charconv>
+#include<ostream>
+#include<istream>
 
-#include"utils.hpp"
-#include"recipe.hpp"
-#include"parser.hpp"
 #include"searcher.hpp"
 #include"commands.hpp"
-
-using namespace parser;
-using namespace parser::literals;
 
 struct Interpreter
 {
 	struct Command
 	{
-		using Argument = std::variant<Value,std::string>;
-
 		std::string type;
-		std::vector<std::variant<Value,std::string>> arguments;
+		commands::Commands::Definition::ArgumentList arguments;
 	};
 
 	std::istream &in;
@@ -29,47 +22,9 @@ struct Interpreter
 
 	Interpreter(std::istream& in, std::ostream& out):in(in),out(out){}
 
-	Command parse_command(std::istream& in)
-	{
-		Command command;
-		std::string line;
-		std::getline(in,line);
-		
-		auto number = FromChars<float>();
-		auto string = Quoted() | Quoted('\'');
-		auto identifier = "[^ \t\n\r'\"][^ \t\n\r]*"_re;
-		auto commands = utils::map(commands::Commands::definitions, [](auto definition){return definition.name;});
-		
-		auto tokens = (Keyword::keywords(commands)+*(number|identifier|string)).parse(line);
+	Command parse_command(std::istream& in);
 
-		command.type = tokens.get<0>().match;
-		
-		auto arguments = utils::map(tokens.get<1>().parsed,[=](auto argument)->commands::Commands::Definition::Argument{
-			if(argument == number){
-				return Value(std::any_cast<float>(argument.value));
-			}else if(argument == string){
-				return Value(std::any_cast<std::string>(argument.value));
-			}else if(argument == identifier){
-				return argument.match;
-			}else{
-				throw "unexpected token format";
-			}
-		});
-		command.arguments.assign(arguments.begin(),arguments.end());
+	void execute_command(Command &command, std::ostream& out);
 
-		return command;
-	}
-
-	void execute_command(Command &command, std::ostream& out)
-	{
-		commands::Commands::find(command.type)->execute(searcher,command.arguments);
-	}
-
-	void run()
-	{
-		for(;;){
-			auto command = parse_command(in);
-			execute_command(command,out);
-		}
-	}
+	void run();
 };
